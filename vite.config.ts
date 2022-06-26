475290f86ff4b868c166fc86fda3e663a7ed9583
eetann <hideharu092@gmail.com>
2022-06-26 16:36:38 +0900

    add packages for lint and format

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { crx, defineManifest } from '@crxjs/vite-plugin'
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 const manifest = defineManifest({
   manifest_version: 3,
-  name: 'Bookmark Nickname',
-  version: '1.0.0',
+  name: "Bookmark Nickname",
+  version: "1.0.0",
   permissions: ["bookmarks"],
   action: {
-    default_popup: "index.html"
-  }
-})
+    default_popup: "index.html",
+  },
+});
 
 export default defineConfig({
-  plugins: [
-    react(),
-    crx({ manifest }),
-  ],
-})
+  plugins: [react(), crx({ manifest })],
+});
