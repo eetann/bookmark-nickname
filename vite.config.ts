e54842f9a5754d0262c977cd48f1dd077d349e8d
eetann <hideharu092@gmail.com>
2022-06-26 16:04:29 +0900

    add manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { crx, defineManifest } from '@crxjs/vite-plugin'
 
-// https://vitejs.dev/config/
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'Bookmark Nickname',
+  version: '1.0.0',
+  permissions: ["bookmarks"],
+  action: {
+    default_popup: "index.html"
+  }
+})
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    crx({ manifest }),
+  ],
 })
